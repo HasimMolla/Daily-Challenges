3458423f48016a8d57dd3036d2c890039dc7e78b
Molla1441 <mhasim1441@gmail.com>
2023-11-29 15:36:05 +0530

    add play and pause functionality in my play music project

--- Day-3/script.js
+++ Day-3/script.js
@@ -1,24 +1,24 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const audio = new Audio("arabic_music.mp3");
-//
-//   const button = document.getElementById("btn");
-//
-//   button.addEventListener("click", function () {
-//     if (audio.paused) {
-//       audio.play();
-//       button.innerHTML = "Pause";
-//     } else {
-//       audio.pause();
-//       audio.currentTime = 0;
-//       button.innerHTML = "Play";
-//     }
-//   });
-// });
+document.addEventListener("DOMContentLoaded", function () {
+  const audio = new Audio("arabic_music.mp3");
 
-const audio = new Audio()
-audio.src = "./arabic_music.mp3";
-const button = document.getElementById("btn");
-button.addEventListener('click', function () {
-    audio.play();
-    audio.loop = true;
-})
+  const button = document.getElementById("btn");
+
+  button.addEventListener("click", function () {
+    if (audio.paused) {
+      audio.play();
+      button.innerHTML = "⏸️Pause";
+    } else {
+      audio.pause();
+      audio.currentTime = 0;
+      button.innerHTML = "🎵Play";
+    }
+  });
+});
+
+// const audio = new Audio()
+// audio.src = "./arabic_music.mp3";
+// const button = document.getElementById("btn");
+// button.addEventListener('click', function () {
+//     audio.play();
+//     audio.loop = true;
+// })
